d16571c9085278f740aa6e31d52f4546559de726
tp_learn <tp_study@php.com>
2026-09-13 10:18:37 +0800

    fix: export force-static for opengraph-image routes

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,5 +1,5 @@
 import OpengraphImage from "components/opengraph-image";
-
+export const dynamic = "force-static";
 export default async function Image() {
   return await OpengraphImage();
 }
--- app/search/[collection]/opengraph-image.tsx
+++ app/search/[collection]/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import OpengraphImage from "components/opengraph-image";
 import { getCollection } from "lib/shopify";
-
+export const dynamic = "force-static";
 export default async function Image({
   params,
 }: {
